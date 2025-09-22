3ef20b1d9868b07bf91367aa08cb4dd7b0d7a35a
Sousxs <impessoal.pedro@gmail.com>
2025-09-21 21:56:27 -0300

    lista circular

--- ED1/c/listCircular.c
+++ ED1/c/listCircular.c
@@ -1,4 +1,7 @@
 //
+// Aluno: Pedro Henrique Rocha Souza 
+// Matrícula: 20251002803735
+//
 //  listaCircular.c - Lista ordenada em vetor circular com busca binária
 //
 //  1) Lista circular usando aritmética modular (%) para contornar índices
